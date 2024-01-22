c5987714a3f207a7ec0890235de3769c58a504ef
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-01-22 14:43:41 -0500

    add mongo db

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+//Connect to DB
+const mongoInfo = process.env.DB_KEY;
+mongoose.connect(mongoInfo);
+
+const database = mongoose.connection;
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+database.on('error', (error) => {
+    console.log(error)
+})
 //Import routes
 const authRoute = require('./routes/auth');
 
