8d57b037c6f252f8a6f04e06f6f2dbe5a98c13c3
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-01-22 15:18:46 -0500

    create user model, for db post request post to db

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,19 @@
 //authentication routes
-
+const User = require('../model/User');
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
-    res.send('Register')
-})
-
-
+router.post('/register', async (req, res) => {
+    const user = new User({
+        userName: req.body.userName,
+        password: req.body.password,
+        email: req.body.email
+    });
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch(err) {
+        res.status(400).send(err);
+    }
+});
 
 module.exports = router;
