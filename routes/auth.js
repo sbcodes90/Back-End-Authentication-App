bc28ea156fca9435ee72b94fe8a0949ed823af06
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-01-22 16:17:36 -0500

    install bcrypt encrpyt password,

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,39 @@
 //authentication routes
 const User = require('../model/User');
 const router = require('express').Router();
+const  { registerValidation } = require('../validation');
+//validation
+const Joi = require('@hapi/joi');
+const bcrypt = require('bcryptjs');
+
+const schema = Joi.object({
+    userName: Joi.string().min(3).max(255).required(),
+    password: Joi.string().min(6).max(1024).required(),
+    email: Joi.string().min(10).max(255).required().email()
+
+});
+
+
 
 router.post('/register', async (req, res) => {
+   
+   //validate data before we make a new user
+   const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error);
+
+    //Check if new user is already in the database
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email already exists');
+
+    //encrypt password
+    const complexity = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, complexity);
+
+
+    //Create a new user
     const user = new User({
         userName: req.body.userName,
-        password: req.body.password,
+        password: hashedPassword,
         email: req.body.email
     });
     try {
---
+++ validation.js
@@ -0,0 +1,27 @@
+const Joi = require('@hapi/joi');
+
+//Register Validation
+
+const registerValidation = (data) => {
+    const schema = Joi.object({
+        userName: Joi.string().min(3).max(255).required(),
+        password: Joi.string().min(6).max(1024).required(),
+        email: Joi.string().min(10).max(255).required().email()
+    
+    });
+    return schema.validate(data);
+}
+
+//Login Validation
+
+const loginValidation = (data) => {
+    const schema = Joi.object({
+        userName: Joi.string().min(3).max(255).required(),
+        password: Joi.string().min(6).max(1024).required(),
+    
+    });
+    return schema.validate(data);
+}
+
+module.exports.registerValidation =  registerValidation;
+module.exports.loginValidation =  loginValidation;
