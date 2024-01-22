b1165c589e33fc23015dfe862a5bd2c0df4e3522
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-01-22 18:50:26 -0500

    install tailwind css

--- client/auth-app/src/App.jsx
+++ client/auth-app/src/App.jsx
@@ -28,6 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h1 className="text-3xl font-bold text-red-500 underline text-center">Hello world!</h1> 
+
     </>
   )
 }
