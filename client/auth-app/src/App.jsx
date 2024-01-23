bee9aaa9138d91172246e9fdd27b75b51cee0bbe
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-01-23 18:50:20 -0500

    Revert "install firebase and create sign up page"
    This reverts commit 3291331bdb366629a38457438db8df252bb180d2.

--- client/auth-app/src/App.jsx
+++ client/auth-app/src/App.jsx
@@ -1,12 +1,10 @@
-import SignUp from "./SignUp";
-
-SignUp;
 function App() {
+
   return (
     <>
-      <SignUp />
+      <h1 className="text-3xl font-bold text-red-500 underline text-center  ">Hello world!</h1> 
     </>
-  );
+  )
 }
 
-export default App;
+export default App
