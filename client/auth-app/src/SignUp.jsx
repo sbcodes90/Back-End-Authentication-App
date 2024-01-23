b6c302f6effbe8a8f826229ec5d1287b6b83fd50
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-01-23 14:53:38 -0500

    update sign up page

--- client/auth-app/src/SignUp.jsx
+++ client/auth-app/src/SignUp.jsx
@@ -36,7 +36,20 @@ function SignUp() {
             type="password"
             placeholder="Password"
           />
-          {/* <p className="text-red-500 text-xs italic">Please choose a password.</p> */}
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-lg font-bold mb-2"
+            for="email"
+          >
+            Email Address
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-5 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email Address"
+          />
         </div>
         <div className="flex items-center justify-between px-2">
           <button
